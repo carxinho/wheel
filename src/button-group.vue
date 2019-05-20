a1a116e9da2c69867ceb815af16823966293452b
carxinho <hocarxin@163.com>
2019-05-20 17:14:51 +0800

    检查g-button-group中的子元素除

--- src/button-group.vue
+++ src/button-group.vue
@@ -4,7 +4,16 @@
     </div>
 </template>
 <script>
-export default {};
+export default {
+    mounted() {
+        for(let node of this.$el.children){
+            let name = node.nodeName.toLowerCase()
+            if(name !== 'button'){
+                console.warn(`'g-button-group 的子元素应全为 g-button,而你的是${name}'`)
+            }
+        }
+    }
+};
 </script>
 <style lang="scss">
 .g-button-group {
