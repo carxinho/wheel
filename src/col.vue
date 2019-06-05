a2d91cda2995012216fbd4e326d66ed8d2416e95
carxinho <hocarxin@163.com>
2019-06-05 17:42:24 +0800

    完成col row的基本样式

---
+++ src/col.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="col" :class="`col-${span}`">
+        <slot></slot>
+    </div>
+</template>
+<script>
+export default {
+    props:{
+        span: {
+            type: [Number, String]
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.col {
+  height: 100px;
+  background: gray;
+  border: 1px solid red;
+  width: 50%;
+  $grid-columns: 24;
+  $class-prefix: col-;
+  @for $n from 1 through $grid-columns {
+    &.#{$class-prefix}#{$n} {
+      width: percentage($n / $grid-columns);
+    }
+  }
+}
+</style>
