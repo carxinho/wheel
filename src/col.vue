39e626b4d115c72b57d53404e34ec7c86aa587fa
carxinho <hocarxin@163.com>
2019-06-21 15:36:11 +0800

    修改样式完成基本响应式

--- src/col.vue
+++ src/col.vue
@@ -79,7 +79,7 @@ export default {
       margin-left: percentage($n / $grid-columns);
     }
   }
-  @media (min-width: 576px) and (max-width: 768px) {
+  @media (min-width: 577px ){
     $class-prefix: col-ipad-;
     @for $n from 1 through $grid-columns {
       &.#{$class-prefix}#{$n} {
@@ -93,7 +93,7 @@ export default {
       }
     }
   }
-  @media (min-width: 769px) and (max-width: 992px) {
+  @media (min-width: 769px){
     $class-prefix: col-narrow-pc-;
     @for $n from 1 through $grid-columns {
       &.#{$class-prefix}#{$n} {
@@ -107,7 +107,7 @@ export default {
       }
     }
   }
-  @media (min-width: 993px) and (max-width: 1200px) {
+  @media (min-width: 993px){
     $class-prefix: col-pc-;
     @for $n from 1 through $grid-columns {
       &.#{$class-prefix}#{$n} {
