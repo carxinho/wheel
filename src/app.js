bb02fd68ca3750cc52b45c36438742e750bfd260
carxinho <hocarxin@163.com>
2019-06-23 16:54:28 +0800

    新增 toast 组件，基本实现样式，有bug待改

--- src/app.js
+++ src/app.js
@@ -10,6 +10,8 @@ import Header from './header'
 import Footer from './footer'
 import Sider from './sider'
 import Content from './content'
+import Toast from './toast'
+import plugin from './plugin'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
@@ -22,6 +24,8 @@ Vue.component('g-header', Header)
 Vue.component('g-footer', Footer)
 Vue.component('g-sider', Sider)
 Vue.component('g-content', Content)
+Vue.component('g-toast', Toast)
+Vue.use(plugin)
 
 new Vue({
     el: '#app',
@@ -31,9 +35,12 @@ new Vue({
         loading3: false,
         message: 'hi'
     },
+    created(){
+        
+    },
     methods: {
-        inputChange(e){
-            console.log(e.target.value)
+        showToast(){
+            this.$toast('mdkfskdfksdfklsdlkfmslkdflskdmflks')
         }
     }
 })
---
+++ src/toast.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="toast">
+        <slot></slot>
+    </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss" scoped>
+$font-size: 14px;
+$toast-height: 40px;
+$toast-bg: rgba(0, 0, 0, 0.75);
+.toast {
+  position: fixed;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  font-size: $font-size;
+  color: white;
+  line-height: 1.8;
+  display: inline-flex;
+  align-items: center;
+  padding: 0 16px;
+  height: $toast-height;
+  background: $toast-bg;
+  border-radius: 4px;
+  box-shadow: 0px 0px 3px 0px rgba(0, 0, 0, 0.5);
+}
+</style>
