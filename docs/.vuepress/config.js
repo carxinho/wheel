2a8955799d2e62b91ce2fc88cb23719a5d98cf8c
Carxinho <1760490877@qq.com>
2019-07-14 04:58:41 +0800

    自制官网

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    title: 'Wheel UI',
+    description: '基于 Vue 的 UI 框架',
+    themeConfig: {
+        nav:[
+            {text: '主页', link:'https://github.com/carxinho'},
+            {text: '文档', link:'/guide/'},
+            {text: '交流', link:'https://google.com'}
+        ],
+        sidebar: [
+            {
+                title:'入门',
+                children: [
+                    '/install/',
+                    '/get-started/'
+                ]
+            },
+            {
+                title:'组件',
+                children: [
+                    '/components/button',
+                ]
+            }
+        ]
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,3 @@
+# Wheel UI 官网
+
+Wheel UI 是一个基于 Vue 的 UI 框架，其提供了button / input / popover / tabs / toast / collapse 等多款常用组件，适合移动端以及 PC 端使用。
